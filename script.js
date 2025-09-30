76a5994ce6d1045b6bfb7b2671abfdca23a43e8c
Shazadkhan1999 <Shazadkhan40005@gmail.com>
2025-10-01 00:28:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,27 @@
 class Person {}
 
 class Student extends Person {}
-
-class Teacher extends Person {}
-
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+ class Person {
+	 constructor(name, age){
+		 this._name = name;
+		 this._age = age;
+	 }
+	 //getter
+	 get name() {
+		 return this._name;
+	 }
+	 // setter
+	 set age(newAge){
+			 this._age = newAge;
+		 
+	 }
+ }
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+// // Do not change the code below this line
+// window.Person = Person;
+// window.Student = Student;
+// window.Teacher = Teacher;
